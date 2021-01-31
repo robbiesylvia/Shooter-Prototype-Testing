e9095165ccffd2a394ecef3edb8ffdc8f7e6bbfa
FrostyWolfZ <63287585+FrostyWolfZ@users.noreply.github.com>
2021-01-31 14:08:13 -0800

    Manual voltage update

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Shooter;
 import frc.robot.Constants;
 
+//manually change the targetVoltage in Constants.java
+
     
 public class ShooterCommand extends CommandBase {
     public final Shooter shooter;
@@ -17,32 +19,34 @@ public class ShooterCommand extends CommandBase {
     @Override
     public void initialize() {
         startTime = System.currentTimeMillis();
-        shooter.firstEncoder.reset();
-        shooter.secondEncoder.reset();
+        Constants.timesExecuted = 0;
     }
   
     @Override
     public void execute() { 
-      Constants.timesExecuted += 1;
-      shooter.setVoltage(shooter.firstCurrentVoltage, shooter.secondCurrentVoltage);
+      shooter.firstMotor.setVoltage(Constants.targetVoltage);
+      /* Constants.timesExecuted += 1;
+      shooter.setVoltage();
       double firstError = shooter.firstRPM - (shooter.firstEncoder.getRate() * 60);
+      System.out.println(shooter.firstRPM);
       shooter.firstCurrentVoltage += firstError * Constants.kP;
       double secondError = shooter.secondRPM - (shooter.secondEncoder.getRate() * 60);
       shooter.secondCurrentVoltage += secondError * Constants.kP;
       System.out.println(shooter.firstEncoder.getRate() * 60);
-      System.out.println(shooter.secondEncoder.getRate() * 60);
+      System.out.println(shooter.secondEncoder.getRate() * 60); *
         if(Math.abs(firstError) < 20){
             Shooter.isFirstRPMGood = true;
         }   
         if(Math.abs(secondError) < 20){
             Shooter.isSecondRPMGood = true;
         }
-        System.out.println("Executed " + Constants.timesExecuted + " times.");
+        System.out.println("Executed " + Constants.timesExecuted + " times."); */
     }
   
     @Override
     public void end(boolean interrupted) {
-      System.out.println("Time to reach RPM: " + (System.currentTimeMillis() - startTime));
+      //System.out.println("Time to reach RPM: " + (System.currentTimeMillis() - startTime));
+      shooter.firstMotor.setVoltage(0);
     }
   
     @Override
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -5,13 +5,15 @@ import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+//manually change the targetVoltage in Constants.java
+
 public class Shooter extends SubsystemBase {
     public Talon firstMotor = new Talon(Constants.talonFirstChannel);
     public Talon secondMotor = new Talon(Constants.talonSecondChannel);
-    
-    public double firstRPM;
+
+    public double firstRPM = 200;
     public double firstCurrentVoltage;
-    public double secondRPM;
+    public double secondRPM = 200;
     public double secondCurrentVoltage;
     public static boolean isFirstRPMGood = false;
     public static boolean isSecondRPMGood = false;
@@ -19,20 +21,20 @@ public class Shooter extends SubsystemBase {
     public final Encoder firstEncoder = new Encoder(Constants.firstEncoderPort1, Constants.firstEncoderPort2);
     public final Encoder secondEncoder = new Encoder(Constants.secondEncoderPort1, Constants.secondEncoderPort2);
   
+    //configures encoders, 2048 counts per revolution = 512 pulses per revolution, so multiply pulses by 1/512 to get rotations per second
     public Shooter() {
       firstEncoder.setDistancePerPulse(1./512.);
       firstEncoder.setMaxPeriod(1);
       firstEncoder.setMinRate(1);
       firstEncoder.setSamplesToAverage(5);
       secondEncoder.setDistancePerPulse(1./512.);
-      secondEncoder.setMaxPeriod(1);
-      secondEncoder.setMinRate(1);
+      secondEncoder.setMaxPeriod(0.1);
+      secondEncoder.setMinRate(10);
       secondEncoder.setSamplesToAverage(5);
   }
-  
-//setRPM is successfully cycled
-  public void setVoltage(double firstVoltage, double secondVoltage) {
-      firstMotor.set(firstVoltage);
-      secondMotor.set(secondVoltage);
+  //sets voltage of each motor, switched to voltage from RPM for simplicity
+  public void setVoltage() {
+      firstMotor.setVoltage(firstCurrentVoltage);
+      secondMotor.setVoltage(secondCurrentVoltage);
     }
 }
