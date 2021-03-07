0822eea0b563c19ecc53cdfb6426d5dfacbf5173
Robbie Sylvia <robbie0sylvia@gmail.com>
2021-03-07 13:17:51 -0800

    code minimized, simulation WIP stuff removed (circumstances changed)

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 import edu.wpi.first.wpilibj.RobotBase;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.BaseTalon;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -18,8 +19,7 @@ public class Shooter extends SubsystemBase {
   public Shooter(){ 
     if(RobotBase.isReal()){
 
-    firstMotor = new TalonFX(Constants.talonFirstChannel);
-    
+    firstMotor = new TalonFX(Constants.talonFirstChannel);s
     }else{
       System.out.println("sim activated, testing");
     firstMotor = new TalonSRX(Constants.talonFirstChannel);
@@ -27,6 +27,13 @@ public class Shooter extends SubsystemBase {
 
     }
   }
+  /*targetVelocity is in units/100ms and the integrated encoder is based on 2048 units/revolution, so to convert from targetRPM to targetVelocity, 
+   *(targetRPM) / ((100ms per 1 second = 10) (sec per min = 60)) 
+   */
+
+   //factor in gear ratio? (with wheels) ~*~
+
+  double targetVelocity;
   double currentSensorVelocity;
   double currentRPM;
   public static double currentPercentOutput;
@@ -36,13 +43,12 @@ public class Shooter extends SubsystemBase {
 
     currentRPM = (currentSensorVelocity * 10 * 60)/2048;
     // converts to RPM, 100 ms * 10 = 1 second, * 60 = 1 minute, /2048 because 2048 pulses/revolution
-   
-
     double errorPercentOutput = targetPercentOutput - currentPercentOutput;
     return errorPercentOutput; 
    }
 
    public void PIDControl (BaseTalon baseTalon, double targetPercentOutput) {
+     
     double errorRPM = getVelocityError(baseTalon, targetPercentOutput);
     double gainsPercentOutput = (errorRPM) * Constants.kP;
     double newPercentOutput = currentPercentOutput + gainsPercentOutput;
@@ -53,9 +59,9 @@ public class Shooter extends SubsystemBase {
       System.out.println("Failsafe activated because percent output was set to a value greater than 1 or less than -1. Something is not right here!");
 
     }else{
-    firstMotor.set(ControlMode.PercentOutput, newPercentOutput);
+    firstMotor.set(ControlMode.Velocity, targetVelocity);
     currentPercentOutput = newPercentOutput; 
-    System.out.println("Current percent output =" + currentPercentOutput);
+    System.out.println("Current RPM =" + currentPercentOutput);
     }
    } 
  }  
